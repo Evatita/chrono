99337803dd57b9d1a65d2bac34136a91c8f7030a
bpe2 <bpe2@vincenty>
2015-10-24 22:57:36 +0300

    changes

--- chrono.py
+++ chrono.py
@@ -1,23 +1,50 @@
 #! /usr/bin/python
 
 import sys
+import datetime
 
-def read_cts_file(ct):
-
-  with open(ct, 'r') as buf: 
+'''
+def read_cts_file(self):
+    
+  with open(ct, 'r') as buf:
     line = buf.readline()
-
+    
     while line:
       if line[0] == "#":
+        pass
+      elif line[1] == "+"          
         pass
       else:
         print line
       line = buf.readline()
 
-#   for line in buf:
-#      columns = line.split()
-#      if len(columns) <= 11:
-#        print columns[10]
+class CoordRec():
 
-# ctsfile = sys.argv[1]
-read_cts_file(sys.argv[1])
+  CoordRec_list =[]
+
+  def __init__(self, x, y, z):
+   self.__x = final_x
+   self.__y = final_y
+   self.__z = final_z
+'''
+class CoordRec:
+
+  CoordRec_list = []
+
+  def __init__(self, ctsfile):
+
+    with open(ctsfile, 'r') as buf:
+      line = buf.readline()
+      
+      while line:
+        if line[0] == '#':
+          pass
+        elif line[1] == '+':
+          pass
+        else:
+          print line
+        line = buf.readline
+
+ctsfile(sys.argv[1])
+#ctsfile = sys.argv[1]
+#read_cts_file(sys.argv[1])
