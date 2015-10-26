383ae77103fa2ab64c8f98af9717210588c28d21
bpe2 <bpe2@vincenty>
2015-10-26 12:04:46 +0200

    recent

--- chrono.py
+++ chrono.py
@@ -3,48 +3,33 @@
 import sys
 import datetime
 
-'''
-def read_cts_file(self):
-    
+def read_cts_file(ct):
+
   with open(ct, 'r') as buf:
     line = buf.readline()
     
     while line:
       if line[0] == "#":
         pass
-      elif line[1] == "+"          
+      elif line[1] == "+":          
         pass
       else:
         print line
       line = buf.readline()
 
-class CoordRec():
-
-  CoordRec_list =[]
-
-  def __init__(self, x, y, z):
-   self.__x = final_x
-   self.__y = final_y
-   self.__z = final_z
-'''
 class CoordRec:
-
   CoordRec_list = []
 
-  def __init__(self, ctsfile):
+  def __init__(self, line)
+    global line
+      l = line.split()
+      self.x = l[6]
+      self.y = l[7]
+      self.z = l[8]
+     
 
-    with open(ctsfile, 'r') as buf:
-      line = buf.readline()
-      
-      while line:
-        if line[0] == '#':
-          pass
-        elif line[1] == '+':
-          pass
-        else:
-          print line
-        line = buf.readline
 
-ctsfile(sys.argv[1])
+
+
 #ctsfile = sys.argv[1]
-#read_cts_file(sys.argv[1])
+read_cts_file(sys.argv[1])
