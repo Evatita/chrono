5e10898c8833a8e040becbae67bfb02db0f0b616
eva <eva@localhost.localdomain>
2015-10-30 13:39:01 +0200

    final mean of x,y,z

--- chrono.py
+++ chrono.py
@@ -13,8 +13,6 @@ def read_cts_file(ct):
         while line:
             if line[0] == "#":
                 pass
-            #elif line[1] == "+":          
-            #    pass
             else:
                 try:
                     cr = CoordRec(line)
@@ -23,11 +21,38 @@ def read_cts_file(ct):
                     print 'Invalid line: [%s]; line skipped'%(line)
             line = buf.readline()
 
+#s0 is set to be equal to 1
+
 def compute_average_x(cr_list):
     sum_of_x = 0
+    Pi = 0
     for i in cr_list:
-        sum_of_x = sum_of_x + i.x()
-    return sum_of_x / len(cr_list)
+        sum_of_x = sum_of_x + (i.x() * ( 1 / i.sx()))
+        Pi = Pi + ( 1 / i.sx())
+    return sum_of_x / Pi 
+
+def compute_average_y(cr_list):
+    sum_of_y = 0
+    Pi = 0
+    for i in cr_list:
+        sum_of_y = sum_of_y + (i.y() * (1 / i.sy()))
+        Pi = Pi + ( 1 / i.sy())
+    return sum_of_y / Pi
+
+def compute_average_z(cr_list):
+    sum_of_z = 0
+    Pi = 0
+    for i in cr_list:
+        sum_of_z = sum_of_z + (i.z() * (1 / i.sz()))
+        Pi = Pi + ( 1 / i.sz())
+    return sum_of_z / Pi
+
+#Adjusting optimal line in data x,y: y = a*x +b
+
+def compute_optimal_line(cr_list):
+    dl = matrix()
+
+
 
 class CoordRec:
 
@@ -48,9 +73,35 @@ class CoordRec:
 
     def x(self):
         return self.m_x
+    
+    def y(self):
+        return self.m_y
+    
+    def z(self):
+        return self.m_z
+    
+    def sx(self):
+        return self.m_sx
+    
+    def sy(self):
+        return self.m_sy
      
+    def sz(self):
+        return self.m_sz
+    
 #ctsfile = sys.argv[1]
 read_cts_file(sys.argv[1])
 print 'I have read %i elements'%(len(CoordRec_list))
 x_mean = compute_average_x(CoordRec_list)
 print 'Average value of x = %10.3f'%(x_mean)
+y_mean = compute_average_y(CoordRec_list)
+print 'Average value of y = %10.3f'%(y_mean)
+z_mean = compute_average_z(CoordRec_list)
+print 'Average value of z = %10.3f'%(z_mean)
+
+
+
+
+
+
+
