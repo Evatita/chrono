0cf2b4dab18ec41812450d3a9af6ddd22f10a642
Evatita <root@localhost.localdomain>
2015-12-01 14:26:43 +0200

    equations

---
+++ erqk_equ.py
@@ -0,0 +1,82 @@
+#! /usr/bin/python
+
+''' This is a script where an effort is made in order to choose the stations that are effected by an earthquake.
+    For this reason, two equations are used, where the second one is +0.8 magnitudes higher than the first one. The first equation 
+    is according to Delle Donne et al. and the second one, is the equation running in FODITS extension of Bernese Software. '''
+    
+import sys, os
+import datetime
+import math
+
+Stacoord_list = []
+Merq1_list = []
+Merq2_list = []
+
+def read_sta_file(sta):
+    
+    with open(sta, 'r') as buf:
+        line = buf.readline()
+        
+        for line in buf.readline():
+            try:
+                st = Stacoord(line)
+                Stacoord_list.append(st)
+            except:
+                print 'Invalid line: [%s]; line skipped'%(line)
+        line = buf.readline()                                                     
+    
+''' In this step, the distance derq is given by stations coordinates X and Y from sta_list, and coordinates of earthquake epicenter
+    are given by the user. Next step, the equations for choosing the stations. Equation Merq1 is according to Delle Donne and Merq2 
+    is used by FODITS. Muser is the magnitude given by the user.'''
+    
+def dist_sta(sta_list):
+    X_epi = 0
+    Y_epi = 0
+    derq = 0
+    Merq1 = 0
+    Merq2 = 0
+    for i in sta_list():
+        derq = sqrt(pow((X_epi - i.x()), 2) + pow((Y_epi - i_y()), 2))
+        Merq1 = -6.40 + 2.17 * log10(derq)
+        Merq2 = -5.60 + 2.17 * log10(derq)
+        
+        if Muser >= Merq1:
+            Merq1_list.append()
+        else :
+            Merq2_list.append()
+            
+    return 
+
+
+class Station_list:
+
+    def __init__(self, line):
+        try:
+            l = line.split()
+            self.sta_name = l[0]
+            self.m_x = float(l[1])
+            self.m_y = float(l[2])
+            self.m_z = float(l[3])
+        except:
+            raise RuntimeError('Invalid line [%s]'%line)
+        
+   def sta_name(self):
+       return self.sta_name
+   
+   def x(self):
+       return self.m_x
+   
+   def y(self):
+       return self.m_y
+   
+   def z(self):
+       return self.m_z      
+       
+read_sta_file(sys.argv[1])
+derq = dist_sta(Stacoord_list)
+print ' Station distance from the epicenter is %10.3f =' %(derq)
+Merq1 = dist_sta(Stacoord_list)
+Merq2 = dist_sta(Stacoord_list)
+
+
+
