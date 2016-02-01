0ed55002609eba6f6131629c3a035984f2707845
Evatita <teva291@msn.com>
2016-02-01 14:17:35 +0200

    read .EVL file

---
+++ events.py
@@ -0,0 +1,64 @@
+#! /usr/bin/python
+
+import os, sys
+import datetime as dt
+import numpy as np
+
+## EVL file contains the events from FODITS analysis, i.e. periodic functions, velocities changes,
+## discontinuties and decteted outliers, with the following format:
+## Station(0) Flg Event(1), From Epoch(2), To Epoch(3), Period(4), sN[m](5), sE[m](6), sU[m](7), S, Remark
+## Periodic funtions are remarked as EST PERI, velocities changes as EST RATE, discontinuties as 
+## EST DISC and detected outliers as EST OUTL.
+
+def read_evl(evl_file):
+  
+  with open(evl_file, 'r') as buf:
+    events  = []
+    epochs  = []
+    sN      = []
+    SE      = []
+    sU      = []
+    
+    line = buf.readline()
+    while line:
+      if not line[0] == '-':
+        l = line.split()
+        station       = l[0]
+        evl_peri      = l[1]
+        period        = l[15]
+        residualn     = l[16]
+        residuale     = l[17]
+        residualu     = l[18]
+        l = buf.readline().split()
+        station       = l[0]
+        evl_rate      = l[1]
+        epoch_rate    = l[3] + '-' + l[4] + '-' + l[5] + ':' + int(l[6])*3600 + int(l[7])*60 + int(l[8])
+        residualn     = l[16]
+        residuale     = l[17]
+        residualu     = l[18]
+        l = buf.readline().split()
+        station       = l[0]
+        evl_disc      = l[1]
+        epoch_disc    = l[3] + '-' + l[4] + '-' + l[5] + ':' + int(l[6])*3600 + int(l[7])*60 + int(l[8])
+        residualn     = l[16]
+        residuale     = l[17]
+        residualu     = l[18]
+        l = buf.readline().split()
+        station       = l[0]
+        evl_out       = l[1]
+        epoch_start   = l[3] + '-' + l[4] + '-' + l[5] + ':' + int(l[6])*3600 + int(l[7])*60 + int(l[8])
+        epoch_end     = l[9] + '-' + l[10] + '-' + l[11] + ':' + int(l[12])*3600 + int(l[13])*60 + int(l[14])
+        residualn     = l[16]
+        residuale     = l[17]
+        residualu     = l[18]
+        if evl_peri == evl_rate or evl_peri == evl_disc:
+          raise RuntimeError("Invalid EVL format (events)")
+        events.append(evl_peri, evl_rate, evl_disc, evl_out)
+        sN.append(residualn)
+        sE.append(residuale)
+        sU.append(residualu)
+        line = buf.readline()
+        
+evl_file = sys.argv[1]
+
+sys.exit(0)
